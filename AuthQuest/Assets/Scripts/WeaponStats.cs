1f9f6f75056a29678288e1d14ed7790e5d3e3dcd
tlalka <tlalka@live.unc.edu>
2019-04-09 12:48:07 -0400

    add some basic weapons

---
+++ AuthQuest/Assets/Scripts/WeaponStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStats : MonoBehaviour
+{
+    //these must be set
+    public string weaponName;
+    public int weaponAttack;
+    public int weaponSpeed;//controls attack duration and cooldown, 
+
+    public bool isRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
