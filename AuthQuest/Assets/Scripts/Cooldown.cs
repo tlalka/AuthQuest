cf312257327787543f147adeee6e92068ed33657
Matthew Martin <mattmart@live.unc.edu>
2019-04-27 12:10:55 -0400

    beginning cooldown system
    bar is white and in right place on screen

---
+++ AuthQuest/Assets/Scripts/Cooldown.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cooldown : MonoBehaviour
+{
+     private Transform bar;
+    private Vector3 offset;
+    float currentScale;
+    GameObject player;
+    GameObject theCamera;
+    public GameObject DeathUI;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        theCamera = GameObject.Find("Main Camera");
+        offset = transform.position - theCamera.transform.position;
+        currentScale = 1f;
+        bar = transform.Find("Bar");
+        bar.Find("BarSprite").GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    private void Update()
+    {
+        transform.position = theCamera.transform.position + offset;
+
+        if(currentScale <= 0)
+        {
+            Debug.Log("You are dead!");
+            Destroy(player, 2f);
+            DeathUI.gameObject.SetActive(true);
+        }
+    }
+
+    public void TakeDamage(float damageValueNormalized)
+    {
+        bar.localScale = new Vector3(currentScale - damageValueNormalized, 1f);
+        //Debug.Log("damage value = " + damageValueNormalized);
+       // Debug.Log("currentScale = " + currentScale);
+       // Debug.Log((currentScale - damageValueNormalized) + " damage taken");
+        currentScale = bar.localScale.x;
+        if(currentScale <= .3)
+        {
+            bar.Find("BarSprite").GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        else
+        {
+            bar.Find("BarSprite").GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        if(currentScale <= 0)
+        {
+            bar.localScale = new Vector3(0f, 1f);
+            //Destroy(player);
+        }
+    }
+
+    public void HealthRegenerate(float regenValueNormalized)
+    {
+        bar.localScale = new Vector3(currentScale + regenValueNormalized, 1f);
+        currentScale = bar.localScale.x;
+        if (currentScale <= .3)
+        {
+            bar.Find("BarSprite").GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        else
+        {
+            bar.Find("BarSprite").GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        if (currentScale <= 0)
+        {
+            bar.localScale = new Vector3(0f, 1f);
+            //Destroy(player);
+        }
+    }
+}
