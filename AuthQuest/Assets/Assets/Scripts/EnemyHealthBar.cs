32d1d05463781b60a8adc4c7839adfc751701c81
Jack <jackhc@live.unc.edu>
2019-04-27 15:06:40 -0400

    attack works finally

---
+++ AuthQuest/Assets/Assets/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    private Transform bar;
+    private Vector3 offset;
+    float currentScale;
+    private Transform barBackground;
+    private Transform barBorder;
+    GameObject enemy;
+    //GameObject theCamera;
+    //public GameObject DeathUI;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        enemy = transform.parent.gameObject;
+        //theCamera = GameObject.Find("Main Camera");
+        transform.position = new Vector3(enemy.transform.position.x, enemy.transform.position.y + 1);
+        offset = transform.position - enemy.transform.position;
+        //offset.x = transform.position.x - enemy.transform.position.x;
+        //offset.y = transform.position.y - (enemy.transform.position.y + 1);
+        currentScale = 1f;
+        bar = transform.GetChild(1);
+        barBackground = transform.GetChild(0);
+        barBorder = transform.GetChild(2);
+        bar.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+        barBackground.GetComponent<SpriteRenderer>().enabled = false;
+        barBorder.GetComponent<SpriteRenderer>().enabled = false;
+
+    }
+
+    private void Update()
+    {
+        transform.position = enemy.transform.position + offset;
+
+        
+    }
+
+    public void TakeDamage(float damageValueNormalized)
+    {
+        bar.localScale = new Vector3(currentScale - damageValueNormalized, 1f);
+        //Debug.Log("damage value = " + damageValueNormalized);
+       // Debug.Log("currentScale = " + currentScale);
+       // Debug.Log((currentScale - damageValueNormalized) + " damage taken");
+        currentScale = bar.localScale.x;
+        if(currentScale <= .3)
+        {
+            bar.GetChild(0).GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        else
+        {
+            bar.GetChild(0).GetComponent<SpriteRenderer>().color = Color.green;
+        }
+        if(currentScale < 1f) {
+            bar.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+            barBackground.GetComponent<SpriteRenderer>().enabled = true;
+            barBorder.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        if(currentScale <= 0)
+        {
+            bar.localScale = new Vector3(0f, 1f);
+            Destroy(enemy);
+        }
+    }
+}
---
+++ AuthQuest/Assets/Assets/Scripts/UIAppear.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIAppear : MonoBehaviour
+{
+    //[SerializeField] private Image customImage;
+    GameObject player;
+    public GameObject Image;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Image.gameObject.SetActive(true);
+            Debug.Log("You have touched it!");
+        } else
+        {
+            Debug.Log("OnCollisionEnter2D");
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Image.gameObject.SetActive(false);
+        }
+    }
+
+    public void clickCombo()
+    {
+        Debug.Log("You received the sword");
+    }
+
+    public void clickForceLock()
+    {
+        Debug.Log("The chest is broken!");
+    }
+}
