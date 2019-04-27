704ae7373be8fe809739aec4dc32efa23a5b9b27
tlalka <tlalka@live.unc.edu>
2019-04-27 16:22:51 -0400

    enemies drop health

---
+++ AuthQuest/Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    public GameObject heart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //levelmanager = GameObject.FindWithTag("levelM");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDestroy()
+    {
+        Debug.Log("enemy was killed");
+        Vector3 position = this.transform.position;
+        Instantiate(heart, position, Quaternion.identity);
+    }
+}
